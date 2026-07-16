d229f8600f3ebc37f46cc9ddaffc663bf233fce6
noahbkrm <noahbkrm@gmail.com>
2026-07-15 22:55:08 -0700

    working on fusing patient tokens from various embeddings

--- src/model/fusion.py
+++ src/model/fusion.py
@@ -11,15 +11,27 @@ from cnv_encoder import CNVEmbedding
 from snv_encoder import SNVEmbedding
 from clinical_encoder import ClinicalEmbedding
 from rna_encoder import RnaEmbedding
-
-def fuse_tokens(
-        clinical_tokens: torch.Tensor,
-        rna_tokens: torch.Tensor,
-        cnv_tokens: torch.Tensor,
-        snv_tokens: torch.Tensor,
-    ):
-
-    combined_tokens = torch.cat([clinical_tokens, rna_tokens, cnv_tokens, snv_tokens], dim = 1)
-    return combined_tokens
     
+class TokenEmbedding(nn.Module):
+    def __init__(self, hidden_dim: int = HIDDEN_DIM):
+        self.register_buffer(  # Static so it won't be treated as a parameter to be optimized
+            "modality_embedding",
+            nn.Embedding(4, hidden_dim)
+        )
+    
+    def forward(
+            self,
+            clinical_tokens: torch.Tensor,
+            rna_tokens: torch.Tensor,
+            cnv_tokens: torch.Tensor,
+            snv_tokens: torch.Tensor,
+        ):
+
+        clinical_tokens = clinical_tokens + self.modality_embedding
+        rna_tokens = rna_tokens + self.modality_embedding
+        cnv_tokens = cnv_tokens + self.modality_embedding
+        snv_tokens = snv_tokens + self.modality_embedding
+
+        combined_tokens = torch.cat([clinical_tokens, rna_tokens, cnv_tokens, snv_tokens], dim = 1)
+        return combined_tokens
 
