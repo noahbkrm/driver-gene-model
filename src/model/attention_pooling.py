fixing attention pooling bug and adding mixed precision

src/model/attention_pooling.py
@@ -8,16 +8,19 @@ from dataclasses import dataclass
 from fusion import TokenEmbedding
 
 class AttentionPooling(nn.Module):
-    def __init__(self, hidden_dim: int = HIDDEN_DIM, batch_size: int = BATCH):
+    def __init__(self, hidden_dim: int = HIDDEN_DIM):
         super().__init__()
-        self.query = nn.Parameter(torch.randn(batch_size, 1, hidden_dim)) # q, "What information is useful for summarizing this patient?"
-        self.Wq = nn.Parameter(torch.randn(768, 768))
-        self.Wk = nn.Parameter(torch.randn(768, 768))
-        self.Wv = nn.Parameter(torch.randn(768, 768))
+        self.query = nn.Parameter(torch.randn(1, 1, hidden_dim)) # q, "What information is useful for summarizing this patient?"
+        self.Wq = nn.Parameter(torch.randn(hidden_dim, hidden_dim))
+        self.Wk = nn.Parameter(torch.randn(hidden_dim, hidden_dim))
+        self.Wv = nn.Parameter(torch.randn(hidden_dim, hidden_dim))
         self.layernorm = nn.LayerNorm(hidden_dim)
     
     def forward(self, input_tokens): # input token embedding dims: (batch, n_tokens, hidden_dim)
-        Q = torch.matmul(input_tokens, self.Wq) # Q: q*Wq    dims: (batch, 1, hidden_dim)
+        B = input_tokens.size(0)
+        query = self.query.expand(B,-1,-1)
+
+        Q = torch.matmul(query, self.Wq) # Q: q*Wq    dims: (batch, 1, hidden_dim)
         K = torch.matmul(input_tokens, self.Wk) # K: x*Wk    dims: (batch, n_tokens, hidden_dim)
         V = torch.matmul(input_tokens, self.Wv) # V: x*Wv    dims: (batch, n_tokens, hidden_dim)
         sim_matrix = torch.matmul(Q, torch.transpose(K, -2, -1))

src/model/patient_model.py
@@ -21,7 +21,7 @@ class PatientModel(nn.Module):
         self.clinical_encoder = ClinicalEmbedding(hidden_dim)
         self.rna_encoder = RnaEmbedding(rna_stats, hidden_dim)
         self.combine_tokens = TokenEmbedding(hidden_dim)
-        self.attention_pooling = AttentionPooling(hidden_dim, batch_size)
+        self.attention_pooling = AttentionPooling(hidden_dim)
 
     def forward(self, batch, mask_snv = False):
         clinical_tokens = self.clinical_encoder(

src/model/train_jepa.py
@@ -64,6 +64,7 @@ def JEPATraining(
         predictor_model: Predictor,
         loader: DataLoader,
         optimizer,
+        scaler,
         ema_param: float = EMA_PARAM,
     ):
 
@@ -74,23 +75,26 @@ def JEPATraining(
     total_loss = 0
     for batch in loader:
 
+        optimizer.zero_grad()
+
         batch = {
             key: value.to(device, non_blocking=True)
             for key, value in batch.items()
         }
+        with torch.amp.autocast("cuda"):
+            with torch.no_grad():
+                z_target = target_model(batch, mask_snv = False) # Calculate target
 
-        with torch.no_grad():
-            z_target = target_model(batch, mask_snv = False) # Calculate target
-        z_context_raw = context_model(batch, mask_snv = True) # Calculate context with masking
-        z_context_pred = predictor_model(z_context_raw) # Pass conext to predictor to for stabilization
+            z_context_raw = context_model(batch, mask_snv = True) # Calculate context with masking
+            z_context_pred = predictor_model(z_context_raw) # Pass conext to predictor to for stabilization
 
-        optimizer.zero_grad()
+            # Compute MSE
+            loss = mse_loss_function(z_context_pred, z_target)
 
-        # Compute MSE
-        loss = mse_loss_function(z_context_pred, z_target)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
 
-        loss.backward()
-        optimizer.step()
         update_target_model(target_model, context_model, ema_param)
         total_loss += loss.item()
     
@@ -238,6 +242,16 @@ if __name__ == "__main__":
         n_variant_genes=n_variant_genes
     )
 
+    smoke_test(
+        target_model,
+        context_model,
+        predictor,
+        loader,
+        optimizer
+    )
+
+    """ scaler = torch.amp.GradScaler("cuda")
+
     for epoch in range(NUM_EPOCHS):
         loss = JEPATraining(
             target_model,
@@ -245,6 +259,7 @@ if __name__ == "__main__":
             predictor,
             loader,
             optimizer,
+            scaler,
         ) 
-        print(f"Epoch {epoch+1}/{NUM_EPOCHS}, Loss: {loss:.4f}")
+        print(f"Epoch {epoch+1}/{NUM_EPOCHS}, Loss: {loss:.4f}") """
         